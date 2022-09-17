2197308bf0f011fd93d1cd6faf35c0eb288d3177
Fabiola Suarez <fsuarez@technisys.com>
2022-09-17 12:45:30 -0300

    Doxygen

--- TP1/src/operaciones.h
+++ TP1/src/operaciones.h
@@ -1,11 +1,51 @@
 #ifndef OPERACIONES_H_
 #define OPERACIONES_H_
 
+/**
+ * @fn void ingresarCostos(void)
+ * @brief Opción 1 del menú. Acá se ingresan todos los costos de mantenimiento.
+ * @param void No recibe parámetros
+ * @return No retorna nada
+ * */
 void ingresarCostos(void);
+/**
+ * @fn void cargarJugadores(void)
+ * @brief Opción 2 del menú. Acá se ingresan jugadores y se selecciona camiseta, posición y confederación.
+ * @param void No recibe parámetros
+ * @return No retorna nada
+ * */
 void cargarJugadores(void);
+/**
+ * @fn void calcularResultados(float*, float*, float*, float*, float*, float*, int*, int*, int*)
+ * @brief Opción 3 del menú. Acá se hacen todos los cálculos.
+ * @param pAfc Dirección de memoria donde se va a guardar el porcentaje de jugadores de esa confederación.
+ * @param pCaf Dirección de memoria donde se va a guardar el porcentaje de jugadores de esa confederación.
+ * @param pConcacaf Dirección de memoria donde se va a guardar el porcentaje de jugadores de esa confederación.
+ * @param pConmebol Dirección de memoria donde se va a guardar el porcentaje de jugadores de esa confederación.
+ * @param pUefa Dirección de memoria donde se va a guardar el porcentaje de jugadores de esa confederación.
+ * @param pOfc Dirección de memoria donde se va a guardar el porcentaje de jugadores de esa confederación.
+ * @param totalCostos Dirección de memoria donde se va a guardar el total de costos de mantenimiento.
+ * @param totalConAumento Dirección de memoria donde se va a guardar el total de costos con el aumento incluido.
+ * @param aumento Dirección de memoria donde se va a guardar el aumento sobre los precios.
+ * @return No retorna nada
+ * */
 void calcularResultados(float *pAfc, float *pCaf, float *pConcacaf,
 		float *pConmebol, float *pUefa, float *pOfc, int *totalCostos,
 		int *totalConAumento, int *aumento);
+/**
+ * @fn void calcularResultados(float, float, float, float, float, float, int, int, int)
+ * @brief Opción 4 del menú. Acá se muestran todos los cálculos.
+ * @param pAfc El porcentaje de jugadores de esa confederación.
+ * @param pCaf El porcentaje de jugadores de esa confederación.
+ * @param pConcacaf El porcentaje de jugadores de esa confederación.
+ * @param pConmebol El porcentaje de jugadores de esa confederación.
+ * @param pUefa El porcentaje de jugadores de esa confederación.
+ * @param pOfc El porcentaje de jugadores de esa confederación.
+ * @param totalCostos El total de costos de mantenimiento.
+ * @param totalConAumento El total de costos con el aumento incluido.
+ * @param aumento El aumento sobre los precios.
+ * @return No retorna nada
+ * */
 void informarResultados(float pAfc, float pCaf, float pConcacaf,
 		float pConmebol, float pUefa, float pOfc, int totalCostos,
 		int totalConAumento, int aumento);
